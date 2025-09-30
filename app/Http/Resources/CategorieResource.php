5d9b71d262aada130b6036fe8cd6e53bea704c1a
Dibakala-Muke <dibakala90@gmail.com>
2025-09-30 19:08:08 +0100

    Ajout de l'attribut id dans CategorieResource

--- app/Http/Resources/CategorieResource.php
+++ app/Http/Resources/CategorieResource.php
@@ -16,6 +16,7 @@ class CategorieResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            "id" => $this->id,
             "name" => $this->name,
             "slug" => $this->slug,
             "categorie_count" => Categorie::count()
